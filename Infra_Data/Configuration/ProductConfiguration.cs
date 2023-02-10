d03f2a50806c15365fda537e2b64ae8c96e39698
rafael.barbosa <rafael.barbosa@itriad.org.br>
2023-02-10 09:48:31 -0400

    insert product config

---
+++ Infra_Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra_Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+           builder.HasKey(x => x.Id);
+           builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+           builder.Property(x => x.Description).HasMaxLength(100).IsRequired();
+        }
+    }
+}
