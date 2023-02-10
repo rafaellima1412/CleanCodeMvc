203f94c652cd78b168f77b27b8bc2c4ea0beb8d5
rafael.barbosa <rafael.barbosa@itriad.org.br>
2023-02-10 12:02:53 -0400

    create interfaces

---
+++ Aplication/Interfaces/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Interfaces
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetListAsync();
+        Task<T> GetByIdAsync(int? id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(int? id);
+    }
+}
---
+++ Aplication/Interfaces/IProductVwService.cs
@@ -0,0 +1,13 @@
+﻿using Aplication.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Interfaces
+{
+    public  interface IProductVwService : IGenericService<ProductVm>
+    {
+    }
+}
