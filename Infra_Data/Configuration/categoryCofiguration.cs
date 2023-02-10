cb72ac66ebe24fc3477fcb340744164b20268f7b
rafael.barbosa <rafael.barbosa@itriad.org.br>
2023-02-10 09:43:07 -0400

    insert category

---
+++ Infra_Data/Configuration/categoryCofiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra_Data.Configuration
+{
+    public class categoryCofiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+            builder.HasData(
+                new Category(1, "Eletronics"),
+                new Category(2, "Smartphones"),
+                new Category(3, "Books")
+                );
+        }
+    }
+}
---
+++ Infra_Data/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra_Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptionsBuilder<AppDbContext> options) : base() { }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
